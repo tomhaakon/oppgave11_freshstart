34f297dc2c4ccf75109bc4292f88eea17ce576c9
Tom Fjeldheim <tom.fjeldheim@googlemail.com>
2023-05-24 22:14:11 +0200

    save for reboot

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,5 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar app color="white" flat>
-      <v-container class="py-0 fill-height">
-        <v-avatar class="mr-10" color="secondary" size="32"></v-avatar>
-
-        <v-btn v-for="link in links" :key="link" text>
-          {{ link }}
-        </v-btn>
-
-        <v-spacer></v-spacer>
-
-        <v-responsive max-width="260">
-          <v-text-field
-            dense
-            flat
-            hide-details
-            rounded
-            solo-inverted
-          ></v-text-field>
-        </v-responsive>
-      </v-container>
-    </v-app-bar>
-
     <v-main class="grey lighten-3">
       <v-container>
         <v-row>
@@ -39,8 +17,6 @@
 
 <script>
 export default {
-  data: () => ({
-    links: ["Dashboard", "Messages", "Profile", "Updates"],
-  }),
+  data: () => ({}),
 };
 </script>
